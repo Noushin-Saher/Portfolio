6741aae83e6fc71c492a59b19c1ad87321895f55
Noushin <mdnausheen864@gmail.com>
2024-08-29 18:34:49 +0530

    third

--- aboutme.js
+++ aboutme.js
@@ -1,5 +1,3 @@
-// script-about-me.js
-
 document.addEventListener('DOMContentLoaded', function() {
     const aboutContent = `
         <div class="row1">
@@ -16,10 +14,10 @@ document.addEventListener('DOMContentLoaded', function() {
         <div class="about-description">
             <div class="row">
                 <div class="col-md-12">
-                    <p>I am Mohammed Noushin Saher, a dedicated third-year B.Tech Computer Science student with a passion for technology and innovation. Possessing a robust blend of technical prowess and effective soft skills, I am adept at problem-solving and collaboration. I am highly focused on achieving academic excellence and constantly seek opportunities to enhance my skills and knowledge. With a proactive approach to learning and a goal-oriented mindset, I am eager to explore and contribute to challenging projects that align with my interests and aspirations in the tech industry. I am currently working on writing a Research Paper on our working project that is A Machine Learning Framework for Tree Survival Prediction to achieve Sustainable Development Goals. Also I am one of the Sustainable development goals club Main Coordinator among 30 students. I held second position in Poster Presentation in Robotics and also held Second Position in coding round in SRKR Technical event.</p>
+                    <p>I am Mohammed Noushin Saher, a dedicated third-year B.Tech Computer Science student with a passion for technology and innovation, possessing a robust blend of technical prowess and effective soft skills. I am highly focused on achieving academic excellence and constantly seek opportunities to enhance my skills and knowledge. I am eager to explore and contribute to challenging projects that align with my interests and aspirations in the tech industry. I am currently working on writing a <span class="highlight">Research Paper</span> on our working project that is A Machine Learning Framework for Tree Survival Prediction to achieve Sustainable Development Goals. Also, I am one of the Sustainable Development Goals club <span class="highlight">Main Coordinator</span> among 30 students. I held second position in Poster Presentation in Robotics and also held Second Position in coding round in SRKR Technical event.</p>
                     <h2>Coding Profiles</h2>
                     <ul class="list-inline">
-                        <li class="list-inline-item"><a href="https://leetcode.com/u/Noushin_Saher/" target="_blank"><i class=""cib-leetcode"></i>Leetcode</a></li><br>
+                        <li class="list-inline-item"><a href="https://leetcode.com/u/Noushin_Saher/" target="_blank"><i class="cib-leetcode"></i>Leetcode</a></li><br>
                         <li class="list-inline-item"><a href="https://www.codechef.com/users/noushin_saher" target="_blank"><i class="fab fa-codechef"></i>Codechef</a></li><br>
                         <li class="list-inline-item"><a href="https://www.hackerrank.com/profile/22b01a05c0" target="_blank"><i class="fab fa-hackerrank"></i>HackerRank</a></li><br>
                         <li class="list-inline-item"><a href="https://codeforces.com/profile/Noushin_Saher" target="_blank"><i class="fab fa-codeforces"></i>Codeforces</a></li><br>
